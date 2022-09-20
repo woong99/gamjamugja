c80ceb352f29adddd7562351c56fa91dbf58315d
Woong99 <dndry1072@naver.com>
2022-09-20 14:29:32 +0900

    프로젝트 생성
    프로젝트 생성

---
+++ src/main/java/com/example/potatowoong/gamjamugja/GamjamugjaApplication.java
@@ -0,0 +1,13 @@
+package com.example.potatowoong.gamjamugja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamjamugjaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamjamugjaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/potatowoong/gamjamugja/GamjamugjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.potatowoong.gamjamugja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamjamugjaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
