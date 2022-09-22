38368e842b205fd2693d871cf9ffb76f3868acbe
Woong99 <dndry1072@naver.com>
2022-09-22 15:45:32 +0900

    JWT 토큰을 위한 설정

---
+++ src/main/java/com/example/potatowoong/gamjamugja/config/JwtSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.example.potatowoong.gamjamugja.config;
+
+import com.example.potatowoong.gamjamugja.jwt.JwtFilter;
+import com.example.potatowoong.gamjamugja.jwt.TokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private final TokenProvider tokenProvider;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
