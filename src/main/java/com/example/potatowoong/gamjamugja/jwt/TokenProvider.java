b1e40a3dfbe27f43888884475ccfe55848c5b735
Woong99 <dndry1072@naver.com>
2022-09-25 16:21:25 +0900

    테스트를 위한 토큰 만료 시간 임시 변경
    테스트 후 복구

--- src/main/java/com/example/potatowoong/gamjamugja/jwt/TokenProvider.java
+++ src/main/java/com/example/potatowoong/gamjamugja/jwt/TokenProvider.java
@@ -26,7 +26,8 @@ public class TokenProvider {
 
     private static final String AUTHORITIES_KEY = "auth";
     private static final String BEARER_TYPE = "bearer";
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+    //    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 3;  // TODO : 테스트 후 수정
     private final Key key;
 
 
