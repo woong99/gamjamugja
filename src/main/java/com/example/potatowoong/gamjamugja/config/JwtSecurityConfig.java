65307a70ade220cb5eee542634a931d8c63a49ef
Woong99 <dndry1072@naver.com>
2022-10-06 16:31:27 +0900

    JwtTokenProvider, JwtTokenFilter 이름 리펙토링

--- src/main/java/com/example/potatowoong/gamjamugja/config/JwtSecurityConfig.java
+++ src/main/java/com/example/potatowoong/gamjamugja/config/JwtSecurityConfig.java
@@ -1,7 +1,7 @@
 package com.example.potatowoong.gamjamugja.config;
 
-import com.example.potatowoong.gamjamugja.jwt.JwtFilter;
-import com.example.potatowoong.gamjamugja.jwt.TokenProvider;
+import com.example.potatowoong.gamjamugja.jwt.JwtTokenFilter;
+import com.example.potatowoong.gamjamugja.jwt.JwtTokenProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -12,12 +12,12 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 // TokenProvider와 JwtFilter를 SecurityConfig에 적용할 때 사용
 public class JwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
 
-    private final TokenProvider tokenProvider;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     // TokenProvider를 주입받아서 JwtFilter를 통해 SpringConfig 안에 필터를 등록하게 되고, 스프링 시큐리티 전반적인 필터에 적용
     public void configure(HttpSecurity http) throws Exception {
-        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        JwtTokenFilter customFilter = new JwtTokenFilter(jwtTokenProvider);
         // addFilterBefore : 지정된 필터 앞에 커스텀 필터를 추가
         http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
     }
