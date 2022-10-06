94b1716a00b86d9f6061ec59452b9bad12aad731
Woong99 <dndry1072@naver.com>
2022-10-06 16:35:26 +0900

    LoginRequestDto 추가 및 수정

---
+++ src/main/java/com/example/potatowoong/gamjamugja/dto/auth/LoginRequestDto.java
@@ -0,0 +1,20 @@
+package com.example.potatowoong.gamjamugja.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LoginRequestDto {
+    private String email;
+    private String password;
+
+    public UsernamePasswordAuthenticationToken toAuthentication() {
+        return new UsernamePasswordAuthenticationToken(email, password);
+    }
+}
