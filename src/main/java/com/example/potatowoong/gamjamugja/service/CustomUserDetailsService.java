bb94de9437dd9ea4a1a16b2121d855a7bb2c98f8
Woong99 <dndry1072@naver.com>
2022-10-06 16:39:48 +0900

    @AuthenticationPrincipal 사용을 위한 리펙토링

--- src/main/java/com/example/potatowoong/gamjamugja/service/CustomUserDetailsService.java
+++ src/main/java/com/example/potatowoong/gamjamugja/service/CustomUserDetailsService.java
@@ -1,39 +1,26 @@
 package com.example.potatowoong.gamjamugja.service;
 
-import com.example.potatowoong.gamjamugja.entity.Member;
-import com.example.potatowoong.gamjamugja.repository.MemberRepository;
+import com.example.potatowoong.gamjamugja.adapter.UserAdapter;
+import com.example.potatowoong.gamjamugja.entity.User;
+import com.example.potatowoong.gamjamugja.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
-
 @Service
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
-    private final MemberRepository memberRepository;
+    private final UserRepository userRepository;
 
     @Override
     // 받은 Email을 통해 user가 실제로 존재하는지 알아보는 메소드
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return memberRepository.findByEmail(username)
-                .map(this::createUserDetails)
-                .orElseThrow(() -> new UsernameNotFoundException(username + " 을 DB에서 찾을 수 없습니다"));
-
-    }
-
-    private UserDetails createUserDetails(Member member) {
-        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(member.getAuthority().toString());
-
-        return new User(
-                String.valueOf(member.getId()),
-                member.getPassword(),
-                Collections.singleton(grantedAuthority)
-        );
+        User user = userRepository
+                .findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(username + " 을 DB에서 찾을 수 없습니다."));
+        ;
+        return new UserAdapter(user);
     }
 }
