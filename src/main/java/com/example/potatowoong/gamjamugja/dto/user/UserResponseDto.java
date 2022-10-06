f6b4c2c7258387d9a63a0ed2ff90c3228c0ef494
Woong99 <dndry1072@naver.com>
2022-10-06 16:37:19 +0900

    UserResponseDto 추가

---
+++ src/main/java/com/example/potatowoong/gamjamugja/dto/user/UserResponseDto.java
@@ -0,0 +1,29 @@
+package com.example.potatowoong.gamjamugja.dto.user;
+
+import com.example.potatowoong.gamjamugja.entity.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UserResponseDto {
+    private String email;
+    private String nickname;
+    private Integer age;
+    private String sex;
+    private String department;
+
+    public static UserResponseDto of(User user) {
+        return UserResponseDto.builder()
+                .email(user.getEmail())
+                .nickname(user.getNickname())
+                .age(user.getAge())
+                .sex(user.getSex())
+                .department(user.getDepartment())
+                .build();
+    }
+}
