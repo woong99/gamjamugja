50e8a3ed82f11c3d40355dd0b3bc16a4430d2f4b
Woong99 <dndry1072@naver.com>
2022-10-06 16:33:51 +0900

    필요없는 라인 제거

--- src/main/java/com/example/potatowoong/gamjamugja/config/SecurityUtil.java
+++ src/main/java/com/example/potatowoong/gamjamugja/config/SecurityUtil.java
@@ -16,7 +16,6 @@ public class SecurityUtil {
         if (authentication == null || authentication.getName() == null) {
             throw new RuntimeException("Security Context에 인증 정보가 없습니다.");
         }
-
         return Long.parseLong(authentication.getName());
     }
 }
