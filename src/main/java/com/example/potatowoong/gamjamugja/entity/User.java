bd664548e70c36021d6d2b314b1b351d83e95a1a
Woong99 <dndry1072@naver.com>
2022-10-10 13:46:45 +0900

    User entity 수정
    컬럼 이름 추가, Article, ArticleComments entity와 연관관계 매핑

--- src/main/java/com/example/potatowoong/gamjamugja/entity/User.java
+++ src/main/java/com/example/potatowoong/gamjamugja/entity/User.java
@@ -1,43 +1,70 @@
 package com.example.potatowoong.gamjamugja.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
 @Builder
 @NoArgsConstructor
 @ToString
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "user_email")
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "user_password")
     @Setter
     private String password;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "user_nickname")
     @Setter
     private String nickname;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "user_age")
     private Integer age;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "user_sex")
     private String sex;
 
-    @Column(nullable = false)
+    @Column(nullable = false, name = "user_department")
     private String department;
 
     @Enumerated(EnumType.STRING) // enum 이름은 DB에 저장
+    @Column(nullable = false, name = "user_authority")
     private Authority authority;
 
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+    @CreatedDate
+    @Column(nullable = false, updatable = false, name = "user_created_at")
+    private LocalDateTime createdAt;
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    @ToString.Exclude
+    @Setter
+    private List<Article> articles = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user")
+    @JsonIgnore
+    @ToString.Exclude
+    @Setter
+    private List<ArticleComment> articleComments = new ArrayList<>();
 
     @Builder
     public User(Long id,
@@ -47,7 +74,10 @@ public class User {
                 Integer age,
                 String sex,
                 String department,
-                Authority authority) {
+                Authority authority,
+                LocalDateTime createdAt,
+                List<Article> articles,
+                List<ArticleComment> articleComments) {
         this.id = id;
         this.email = email;
         this.password = password;
@@ -56,5 +86,8 @@ public class User {
         this.sex = sex;
         this.department = department;
         this.authority = authority;
+        this.createdAt = createdAt;
+        this.articles = articles;
+        this.articleComments = articleComments;
     }
 }
